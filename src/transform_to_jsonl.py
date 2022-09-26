b34e35110af5a54abe749a7c8c71ef7840e78a1c
Vlad <kosmobiker@gmail.com>
2022-09-26 09:08:41 +0000

    update

--- src/transform_to_jsonl.py
+++ src/transform_to_jsonl.py
@@ -10,8 +10,9 @@ DATASET_JSONL_FILE="tmp/test_data.jsonl"
 
 #read data
 test = wr.s3.read_csv([TEST_PATH])
-test.rename(columns = {'comment_text':'input'}, inplace = True)
-test['input'].to_csv(DATASET_CSV_FILE, index=False)
+test.rename(columns = {"comment_text":"inputs"}, inplace = True)
+test["parameters"] = '{"truncation": true}'
+test[["inputs", "parameters"]].to_csv(DATASET_CSV_FILE, index=False)
 
 #transform it in specific wformat for SageMaker
 with open(DATASET_CSV_FILE, "r+") as infile, open(DATASET_JSONL_FILE, "w+") as outfile:
