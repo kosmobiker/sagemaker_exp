f93bf9073f14a9e23da2f4f74bc7e79283e78b8b
Vlad <kosmobiker@gmail.com>
2022-09-27 13:42:55 +0000

    changes

--- src/aux/inference_normal.py
+++ src/aux/inference_normal.py
@@ -8,7 +8,9 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 sigmoid = torch.nn.Sigmoid()
 
 def model_fn(model_dir):
-
+    """
+    Here we define tokenizer and model
+    """
     tokenizer_init = AutoTokenizer.from_pretrained('unitary/toxic-bert')
     model = AutoModelForSequenceClassification.from_pretrained('unitary/toxic-bert').eval().to(device)
     
@@ -16,6 +18,9 @@ def model_fn(model_dir):
 
 
 def input_fn(serialized_input_data, content_type=JSON_CONTENT_TYPE):
+    """
+    Read data from JSON input
+    """
     if content_type == JSON_CONTENT_TYPE:
         input_data = json.loads(serialized_input_data)
         return input_data
@@ -25,34 +30,29 @@ def input_fn(serialized_input_data, content_type=JSON_CONTENT_TYPE):
     
 
 def predict_fn(input_data, models):
-
+    # Initialize models and tokenizer
     model_toxic, tokenizer = models
     sequence = input_data[0] 
-    
     max_length = 512
     tokenized_sequence_pair = tokenizer.encode_plus(sequence,
                                                     max_length=max_length,
                                                     padding='max_length',
                                                     truncation=True,
                                                     return_tensors='pt').to(device)
-    
     # Convert example inputs to a format that is compatible with TorchScript tracing
-    example_inputs = tokenized_sequence_pair['input_ids'], tokenized_sequence_pair['attention_mask']
-    
+    inputs = tokenized_sequence_pair['input_ids'], tokenized_sequence_pair['attention_mask']
+    # Make predictions
     with torch.no_grad():
-        outputs = model(**inputs).logits
+        outputs = model_toxic(*inputs).logits
     probas = sigmoid(outputs).cpu().detach().numpy()[0]
-    return {
-        'toxicity': probas[0],
-        'severe_toxic': probas[1],
-        'obscene': probas[2],
-        'threat': probas[3],
-        'insult': probas[4],
-        'identity_hate': probas[5]
-    }
+    return f"'toxicity': {probas[0]}, 'severe_toxic': {probas[1]}, 'obscene': {probas[2]}, 'threat': {probas[3]}, 'insult': {probas[4]}, 'identity_hate': {probas[5]}"
+
 
 
 def output_fn(prediction_output, accept=JSON_CONTENT_TYPE):
+    """
+    Output of the model turned to JSON
+    """
     if accept == JSON_CONTENT_TYPE:
         return json.dumps(prediction_output), accept
     
