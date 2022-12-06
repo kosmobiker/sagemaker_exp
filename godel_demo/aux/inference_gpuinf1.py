4d9ac209d84ba6330cabbabba8451495587903cd
sagemaker <kosmobiker@gmail.com>
2022-12-06 21:11:31 +0000

    some changes
    inf is broken

--- godel_demo/aux/inference_gpuinf1.py
+++ godel_demo/aux/inference_gpuinf1.py
@@ -21,11 +21,11 @@ def model_fn(model_dir):
     tokenizer_init = AutoTokenizer.from_pretrained(MODEL_NAME)
     model_config = AutoConfig.from_pretrained(MODEL_NAME)
     labels = {k: v for k, v in sorted(model_config.label2id.items(), key=lambda item: item[1])}.keys()
-    compiled_model = os.path.exists(f'{model_dir}/model_neuron.pt')
+    compiled_model = os.path.exists(f'{model_dir}/model.pth')
     if compiled_model:
         import torch_neuron
-        os.environ["NEURONCORE_GROUP_SIZES"] = "1"
-        model = torch.jit.load(f'{model_dir}/model_neuron.pt')
+        os.environ['NEURON_RT_NUM_CORES'] = "4"
+        model = torch.jit.load(f'{model_dir}/model.pth')
     else: 
         model = AutoModelForSequenceClassification.from_pretrained(model_dir).to(device)
     return (model, tokenizer_init, labels)
@@ -39,9 +39,9 @@ def predict_fn(input_data, models):
     inputs = tokenizer(sentences, padding=True, max_length=512, truncation=True, return_tensors='pt').to(device)
     with torch.no_grad():
         model.to(device)
-        outputs = model(**inputs).logits
-        probas = sigmoid(outputs).cpu().detach().numpy().round(3).astype('str')
-    return [dict(zip(labels, probas[i])) for i in range(len(probas))]
+        outputs = model(**inputs)
+        probas = sigmoid(outputs[0]).cpu().detach().numpy().round(3).astype('str')
+        return [dict(zip(labels, probas[i])) for i in range(len(probas))]
 
 
 def output_fn(prediction_output, accept=JSON_CONTENT_TYPE):
